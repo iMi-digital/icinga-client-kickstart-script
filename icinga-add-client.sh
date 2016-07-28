6e83e1712ee24413370ea6ab5461077804c04e57
Saman, Serhat <s.saman@imi.de>
2016-07-28 16:09:21 +0200

    version 2 added parameter functions - direct useable

--- icinga-add-client.sh
+++ icinga-add-client.sh
@@ -12,25 +12,58 @@ PATH_OF_FILES="/home/ssn/Projekte/icinga-add-client/"
 # function to set fqnd
 function fqnd_ft {
 	FQND=$(whiptail --title "FQND of new client" --inputbox "Give me your clients FQND?" $W_WIDTH $W_HEIGHT $FQND 3>&1 1>&2 2>&3)
+	exitstatus=$?
+	if [ $exitstatus = 0 ]; then
+  	:
+	else
+    exit
+	fi
 }
 # function to set ip of fqnd
 function ip_of_fqnd_ft {
 	IP_OF_FQND=$(whiptail --title "IP of your new client" --inputbox "Give me the IP-Adresse of your new client?" $W_WIDTH $W_HEIGHT $IP_OF_FQND 3>&1 1>&2 2>&3)
-	# checks is ip is valid
-	if [[ $IP_OF_FQND =~ ^(([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))\.){3}([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))$ ]]; then
-#		echo ""
+	exitstatus=$?
+	if [ $exitstatus = 0 ]; then
+  	:
 	else
-		whiptail --title "Invalid IP" --msgbox "try again !..." $W_WIDTH $W_HEIGHT
-		ip_of_fqnd_ft
+    exit
 	fi
+	check_ip_valid gui
 }
 # function to set path of files - saveing folder
 function path_of_files_ft {
 	PATH_OF_FILES=$(whiptail --title "Path of file" --inputbox "Path to save new files?" $W_WIDTH $W_HEIGHT $PATH_OF_FILES 3>&1 1>&2 2>&3)
+	exitstatus=$?
+	if [ $exitstatus = 0 ]; then
+  	:
+	else
+    exit
+	fi
+	check_path_of_file_ending
+}
+#function to check if ip is valid
+function check_ip_valid() {
+	# checks if ip is valid
+	if [[ $IP_OF_FQND =~ ^(([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))\.){3}([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))$ ]]; then
+		:
+	else
+		if [[ "$1" = "gui" ]]; then
+			whiptail --title "Invalid IP" --msgbox "try again !..." $W_WIDTH $W_HEIGHT
+			ip_of_fqnd_ft
+		elif [[ "$1" = "silenc" ]]; then
+			:
+		else
+			echo "invalid ip"
+		fi
+		:
+	fi
+}
+
+function check_path_of_file_ending {
 	if [[ $(echo "$PATH_OF_FILES"|grep '/$'|wc -l) -eq 0 ]]; then
 		PATH_OF_FILES="$PATH_OF_FILES/"
 	else
-		echo "test"
+		:
 	fi
 }
 
@@ -40,18 +73,17 @@ function check_if_correct {
 # check if all correct
 	if (whiptail --title "Everything Correct?" --yesno "FQND = $FQND \nIP = $IP_OF_FQND \nPath = $PATH_OF_FILES \n \nare those correct?" $W_WIDTH $W_HEIGHT) then
 		# when settings are fine do: look at the path an check if files already exist with the same name
-		echo""
 		# check if files exist
 		if [[ $(find $PATH_OF_FILES -type f -name "$FQND.conf"|wc -l) -gt 0 ]];then
 			# when files exist do: ask to overwrite
 			if (whiptail --title "Some files already exist" --yesno "Should I overwrite your files Yes or No." $W_WIDTH $W_HEIGHT) then
-	    	echo "You chose Yes. Exit status was $?."
+	    	:
 			else
-	    	echo "Exit, bye bye"
+	    	echo "Bye Bye"
 				exit
 			fi
-		else # files not existing - settings are fine - do: create files
-			whiptail --title "Create files" --msgbox "creating files..." $W_WIDTH $W_HEIGHT
+		else # end of function check_if_correct - files not existing - settings are fine - do: function create files
+			:
 		fi
 	else
 		#if settings are not fine: ask which one are not fine
@@ -91,7 +123,6 @@ function check_if_correct {
 }
 # create / overwrite files
 function create_files {
-
 	echo "object Host \"$FQND\" {
         import \"generic-host\"
         address = \"$IP_OF_FQND\"
@@ -111,7 +142,13 @@ function create_files {
         }
 }" > $PATH_OF_FILES$FQND.conf
 
+if [[ $(find $PATH_OF_FILES -type d -name "$FQND"|wc -l) -gt 0 ]];then
+	# when files exist do nothing
+	:
+else
 	mkdir $PATH_OF_FILES$FQND
+fi
+
 	echo "object Service \"apachestatus\" {
         import \"generic-service\"
         host_name = \"$FQND\"
@@ -228,6 +265,7 @@ function create_files {
         vars.check = \"check_zombie_procs\"
 }" > $PATH_OF_FILES$FQND/zombie_procs.conf
 }
+
 if [ "$#" -eq 0 ] ;then
 
 # start of frotnend
@@ -239,8 +277,25 @@ if [ "$#" -eq 0 ] ;then
 	path_of_files_ft
 # asking if settings are correct
 	check_if_correct
+# creating files
+	whiptail --title "Create files" --msgbox "creating files..." $W_WIDTH $W_HEIGHT
+	create_files
+	whiptail --title "Done" --msgbox "Already finished ;)" $W_WIDTH $W_HEIGHT
 
-
+#elif [[ ("$@" = "-h")  ||  ("$@" = "--help") || ("$@" = "-help")]]; then
+#	echo -e "Usage: icinga-add-client.sh for GUI OR\nGive parameters with flags: -f fqnd  -i ip -p path \ne.g: icinga-newclient.sh -f $FQND -i $IP_OF_FQND -p $PATH_OF_FILES"
 else
-	echo "Usage: icinga-newclient.sh"
+	while getopts f:i:p:h option
+	do
+		case "${option}" in
+	  	f) FQND=${OPTARG};;
+	    i) IP_OF_FQND=${OPTARG}
+			check_ip_valid "nogui";;
+	    p) PATH_OF_FILES=${OPTARG}
+			check_path_of_file_ending;;
+			*) echo -e "Usage: icinga-add-client.sh for GUI OR\nGive parameters with flags: -f fqnd  -i ip -p path \ne.g: icinga-newclient.sh -f server.imi.de -i 123.123.123.123 -p /PATH/TO/SAVE/FILES"
+			exit;;
+		esac
+	done
+	create_files
 fi
